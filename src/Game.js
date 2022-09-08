246ec8c0b92f9ca0b800f717751d123851ebbbca
JC Tapias <jctblock@gmail.com>
2022-09-08 13:31:30 -0400

    Checkpoint

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,9 @@
+import TileMap from "./TileMap.js";
+
+const tileSize = 32;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const tileMap = new TileMap(tileSize);
 
 function gameLoop(){
 
---
+++ src/TileMap.js
@@ -0,0 +1,11 @@
+export default class TileMap{
+
+    constructor(tileSize){
+        this.tileSize = tileSize;
+    }
+
+    draw(){
+        
+    }
+
+}
