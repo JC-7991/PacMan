0b524efd70ac7971dca3cdb1575ec4acee44f076
JC Tapias <jctblock@gmail.com>
2022-09-11 23:30:03 -0400

    Checkpoint

--- src/Pacman.js
+++ src/Pacman.js
@@ -1,3 +1,14 @@
 export default class Pacman{
-    
+
+    constructor(x, y, tileSize, velocity, tileMap){
+
+        this.x = x;
+        this.y = y;
+
+        this.tileSize = tileSize;
+        this.velocity = velocity;
+        this.tileMap = tileMap;
+        
+    }
+
 }
